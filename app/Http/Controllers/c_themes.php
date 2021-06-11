3bf4d0a6490bb53e8e5e4374040958223a200d83
= <=>
2021-06-11 23:33:28 +0700

    sdfsdf

--- app/Http/Controllers/c_themes.php
+++ app/Http/Controllers/c_themes.php
@@ -12,14 +12,14 @@ class c_themes extends Controller
     public function getlist()
     {
         $logo = themes::where('note','logo')->orderBy('id','desc')->get();
-        $banner1 = themes::where('note','1')->orderBy('id','desc')->get();
-        $banner2 = themes::where('note','2')->orderBy('id','desc')->get();
-        $banner3 = themes::where('note','3')->orderBy('id','desc')->get();
+        $slider = themes::where('note','slider')->orderBy('id','desc')->get();
+        $title = themes::where('note','title')->orderBy('id','desc')->get();
+        $banner = themes::where('note','banner')->orderBy('id','desc')->get();
     	return view('admin.themes.list',[
             'logo'=>$logo,
-            'banner1'=>$banner1,
-            'banner2'=>$banner2,
-            'banner3'=>$banner3,
+            'slider'=>$slider,
+            'title'=>$title,
+            'banner'=>$banner,
 		]);
     }
 
@@ -36,22 +36,18 @@ class c_themes extends Controller
     {
         if (isset($Request->id)) {
             foreach ($Request->id as $key => $id) {
-                if (isset($Request->dell[$key]) && $Request->dell[$key]=='on') {
-                    $themes = themes::find($id);
-                    $themes->delete();
-                }else{
-                    $themes = themes::find($id);
-                    $themes->name = $Request->name[$key];
-                    if (isset($Request->file('img')[$key])) {
-                        if(File::exists('data/themes/'.$themes->img)) { File::delete('data/themes/'.$themes->img); } // xóa
-                        $file = $Request->file('img')[$key]; 
-                        $filename = $file->getClientOriginalName();
-                        while(file_exists("data/themes/".$filename)){ $filename = str_random(4)."_".$filename; }
-                        $file->move('data/themes', $filename);
-                        $themes->img = $filename;
-                    }
-                    $themes->save();
+                $themes = themes::find($id);
+                $themes->name = $Request->name[$key];
+                $themes->content = $Request->content[$key];
+                if (isset($Request->file('img')[$key])) {
+                    if(File::exists('data/themes/'.$themes->img)) { File::delete('data/themes/'.$themes->img); } // xóa
+                    $file = $Request->file('img')[$key]; 
+                    $filename = $file->getClientOriginalName();
+                    while(file_exists("data/themes/".$filename)){ $filename = str_random(4)."_".$filename; }
+                    $file->move('data/themes', $filename);
+                    $themes->img = $filename;
                 }
+                $themes->save();
             }
         }
         return redirect('admin/themes/list')->with('Alerts','Thành công');
--- resources/views/admin/themes/list.blade.php
+++ resources/views/admin/themes/list.blade.php
@@ -21,14 +21,15 @@
                     @foreach($logo as $val)
                     <div class="col-md-6">
                         <div class="themes">
-                            <img style="" src="data/themes/{{$val->img}}">
+                            <img src="data/themes/{{isset($val->img)?$val->img:'no_image.jpg'}}">
                             <div class="form-group">
                                 <label>Name</label>
                                 <input type="hidden" name="id[]" value="{{ $val->id }}" />
+                                <input type="hidden" name="content[]" value="{{ $val->id }}" />
                                 <input value="{{ $val->name }}" name="name[]" placeholder="name" type="text" class="form-control">
                                 <div class="iteamaction">
-                                    <input value="{{ $val->img }}" name="img[]" type="file" class="form-control">
-                                    <input name="dell[]" type="hidden" value="" class="form-control">
+                                    <input value="{{ $val->img }}" name="img[]" type="file">
+                                    <!-- <a href="admin/themes/delete/{{$val->id}}"><i class="fas fa-trash-alt"></i></a> -->
                                 </div>
                             </div>
                         </div>
@@ -39,22 +40,23 @@
         </div>
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Slider</h6>
-                <div class="dropdown no-arrow"><a href="admin/themes/add/1">Thêm mới</a></div>
+                <h6 class="m-0 font-weight-bold text-primary">slider</h6>
+                <div class="dropdown no-arrow"><a href="admin/themes/add/slider">Thêm mới</a></div>
             </div>
             <div class="card-body">
                 <div class="row">
-                    @foreach($banner1 as $val)
+                    @foreach($slider as $val)
                     <div class="col-md-6">
                         <div class="themes">
-                            <img style="" src="data/themes/{{$val->img}}">
+                            <img src="data/themes/{{isset($val->img)?$val->img:'no_image.jpg'}}">
                             <div class="form-group">
                                 <label>Name</label>
                                 <input type="hidden" name="id[]" value="{{ $val->id }}" />
+                                <input type="hidden" name="content[]" value="{{ $val->id }}" />
                                 <input value="{{ $val->name }}" name="name[]" placeholder="name" type="text" class="form-control">
                                 <div class="iteamaction">
-                                    <input value="{{ $val->img }}" name="img[]" type="file" class="form-control">
-                                    <label class="btn-danger"><input name="dell[]" value="on" type="checkbox" class="form-control"><span>Xóa</span></label>
+                                    <input value="{{ $val->img }}" name="img[]" type="file">
+                                    <a href="admin/themes/delete/{{$val->id}}"><i class="fas fa-trash-alt"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -65,22 +67,50 @@
         </div>
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Banner homes</h6>
-                <!-- <div class="dropdown no-arrow"><a href="admin/themes/add/2">Thêm mới</a></div> -->
+                <h6 class="m-0 font-weight-bold text-primary">title</h6>
+                <!-- <div class="dropdown no-arrow"><a href="admin/themes/add/title">Thêm mới</a></div> -->
             </div>
             <div class="card-body">
                 <div class="row">
-                    @foreach($banner2 as $val)
+                    @foreach($title as $val)
                     <div class="col-md-6">
                         <div class="themes">
-                            <img style="" src="data/themes/{{$val->img}}">
+                            <img src="data/themes/{{isset($val->img)?$val->img:'no_image.jpg'}}">
                             <div class="form-group">
-                                <label>Name</label>
+                                <label>Name ({{ $val->id }})</label>
                                 <input type="hidden" name="id[]" value="{{ $val->id }}" />
                                 <input value="{{ $val->name }}" name="name[]" placeholder="name" type="text" class="form-control">
                                 <div class="iteamaction">
-                                    <input value="{{ $val->img }}" name="img[]" type="file" class="form-control">
-                                    <label class="btn-danger"><input name="dell[]" value="on" type="checkbox" class="form-control"><span>Xóa</span></label>
+                                    <input value="{{ $val->img }}" name="img[]" type="file">
+                                    <!-- <a href="admin/themes/delete/{{$val->id}}"><i class="fas fa-trash-alt"></i></a> -->
+                                </div>
+                                <textarea rows="5" type="text" name="content[]" class="form-control">{!! $val->content !!}</textarea>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">banner</h6>
+                <div class="dropdown no-arrow"><a href="admin/themes/add/banner">Thêm mới</a></div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    @foreach($banner as $val)
+                    <div class="col-md-6">
+                        <div class="themes">
+                            <img src="data/themes/{{isset($val->img)?$val->img:'no_image.jpg'}}">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="hidden" name="id[]" value="{{ $val->id }}" />
+                                <input type="hidden" name="content[]" value="{{ $val->id }}" />
+                                <input value="{{ $val->name }}" name="name[]" placeholder="name" type="text" class="form-control">
+                                <div class="iteamaction">
+                                    <input value="{{ $val->img }}" name="img[]" type="file">
+                                    <a href="admin/themes/delete/{{$val->id}}"><i class="fas fa-trash-alt"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -89,17 +119,17 @@
                 </div>
             </div>
         </div>
+
+
+
     </div>
     
 </div>
 </form>
 <style type="text/css">
-    .themes{display: flex; box-shadow: 0 5px 10px 0; padding: 10px;}
+    .themes{display: flex; box-shadow: 0 5px 10px 0; padding: 10px; margin-bottom: 20px}
     .themes img{height: 100px; margin-right: 10px; box-shadow: 0 1px 2px 0}
     .themes .form-group{width: 100%}
-    .iteamaction{display: flex; margin-top: 10px}
-    .iteamaction label{display: flex;align-items: center;margin: 0 0 0 20px;border: 1px solid #222; border-radius: 5px; color: #fff}
-    .iteamaction input[type="checkbox"]{width: 31px;}
-    .iteamaction span{padding: 0 5px;}
+    .iteamaction{display: flex; margin-top: 10px; justify-content: space-between;align-items: center; margin-bottom: 10px}
 </style>
 @endsection
